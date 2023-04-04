5f212f491d6562ea5825adcd358da224a56a5784
nico17022001 <nico17022001boolean@gmail.com>
2023-04-04 15:55:57 +0200

    pari-dispari-finito

--- dispari/script.js
+++ dispari/script.js
@@ -1,40 +1,18 @@
 const pariDispari = prompt("scegli pari o dispari");
 const numeroScelto = parseInt(prompt("scegli un numero da 1 a 5"));
-const div = document.querySelector("div")
 const numeroPc = generaNumero(1,5);
-console.log(numeroPc)
-
-function generaNumero(min,max) {
-  const numeroGenerato = Math.floor(Math.random() * max) + min;
-  return numeroGenerato
-}
-
-if (numeroScelto > 0 && numeroScelto <= 5) {
-
-}else{
-  prompt("inserisci un numero valido")
-}
 
 const sommaNumeri = numeroScelto + numeroPc
 
+console.log('numero scelto dal pc', numeroPc)
+console.log('somma', sommaNumeri);
 
-if (risultato = (!sommaNumeri % 2)) {
-  let risultato = console.log("dispari")
-}else{
-  let risultato = console.log("pari")
+risultato = sommaNumeri % 2 ? "dispari" : "pari";
+document.querySelector("div").innerHTML = risultato == pariDispari ? "hai vinto" : "hai perso";
+
+function generaNumero(min, max) {
+  return Math.floor(Math.random() * max) + min;
 }
-
-if (risultato == pariDispari) {
-  div.innerHTML = "hai vinto"
-}else{
-  div.innerHTML = "hai perso"
-}
-
-
-
-
-
-
 
 
 
